2be487e06a81f8b9a180e0c6a2ab6ac10c70e956
Odilitime <janesmith@airmail.cc>
2017-12-20 14:50:23 +0000

    revert to older commit

--- components/settings/ImageUpload.vue
+++ components/settings/ImageUpload.vue
@@ -2,31 +2,35 @@
   <div>
     <h3 class="card-title">
       Image upload(using imgur)
-      <small class="text-danger">Deprecated</small>
+      <small class="text-warning">experimental</small>
     </h3>
     <p class="card-text">
-      This feature is deprecated. In current version,  implemented by files API. It is possible for <a href="https://pnut.io/account/support" target="_new">payment users</a> to use this feature.
+      You can attach images if login with imgur.
+      Login info are stored in your browser(Local storage).
     </p>
-    <div v-if="imgur">
-      <p class="card-text">
-        If you would like to remove imgur's token in your browser, press the button below.
-      </p>
-      <p class="card-text">
-        Your current token: {{imgur.access_token}}
-      </p>
-      <button @click="logout" class="btn btn-outline-primary">Remove token</button>
+    <div>
+      <div v-if="!imgur">
+        <button @click="login" class="btn btn-primary">Login with imgur</button>
+      </div>
+      <div v-else>
+        <p class="card-text">
+          Your current token: {{imgur.access_token}}
+        </p>
+        <button @click="logout" class="btn btn-outline-primary">Remove token</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+const IMGUR_LOGIN = 'imgur-login'
 export default {
-  data() {
+  data () {
     return {
       imgur: undefined
     }
   },
-  mounted() {
+  mounted () {
     try {
       const tokenStr = localStorage.getItem('imgur')
       if (tokenStr) { this.imgur = JSON.parse(tokenStr) }
@@ -34,7 +38,18 @@ export default {
     }
   },
   methods: {
-    logout() {
+    login () {
+      const wnd = window.open('/imgur/login', IMGUR_LOGIN, 'resizable=yes,scrollbars=yes')
+      wnd.addEventListener('unload', this.setToken)
+      wnd.addEventListener('beforeunload', this.setToken)
+    },
+    setToken () {
+      const imgur = localStorage.getItem('imgur')
+      this.imgur = imgur
+        ? JSON.parse(imgur)
+        : null
+    },
+    logout () {
       localStorage.removeItem('imgur')
       this.imgur = null
     }
