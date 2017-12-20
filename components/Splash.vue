0e9d0c444649924b61d6e84eaee2278747c0d2ff
Odilitime <janesmith@airmail.cc>
2017-12-20 14:47:16 +0000

    remove pnut verbiage

--- components/Splash.vue
+++ components/Splash.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="text-center">
     <h1 class="display-4">Welcome to Beta!</h1>
-    <p class="lead">Web client for <a href="https://pnut.io">pnut.io</a>.</p>
+    <p class="lead">Web client for <a href="https://tavrn.gg">tavrn.gg</a>.</p>
     <p>
       <a href="/login" class="btn btn-primary btn-lg">
-        Login with pnut.io
+        Login with tavrn.gg
         <i class="fa fa-chevron-right"></i>
       </a>
     </p>
