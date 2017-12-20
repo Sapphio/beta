6af9ecdf2ed8a7f4933f7b10fedd28976b4f9121
Odilitime <janesmith@airmail.cc>
2017-12-20 14:51:21 +0000

    reorganize sidebar, remove settings, link to accounts.sapphire.moe for settings

--- components/sidebar/Settings.vue
+++ components/sidebar/Settings.vue
@@ -1,27 +1,37 @@
 <script>
 import Sidebar from './Sidebar'
 
-export default {
-  extends: Sidebar,
-  data() {
-    const menus = [
+/*
       {type: 'heading', label: 'User'},
       {
         label: 'Account',
         icon: 'fa-user',
         url: '/settings'
       },
+*/
+
+export default {
+  extends: Sidebar,
+  data() {
+    const menus = [
       {type: 'heading', label: 'App'},
+      {
+        label: 'Notification',
+        icon: 'fa-bell',
+        url: '/settings/notification'
+      },
       {
         label: 'Stream',
         icon: 'fa-list',
         url: '/settings/stream'
       },
+      {type: 'heading', label: 'User'},
       {
-        label: 'Notification',
-        icon: 'fa-bell',
-        url: '/settings/notification'
-      }
+        label: 'Account',
+        icon: 'fa-user',
+        normal: true,
+        url: 'https://accounts.sapphire.moe/app/users/my'
+      },
     ]
     return {
       menus
