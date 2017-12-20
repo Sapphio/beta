0f9e4ce29fe7bcf4c2065bb35b3370dd700790fc
Odilitime <janesmith@airmail.cc>
2017-12-20 14:51:50 +0000

    new page for explore feed

---
+++ pages/most-starred.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <h3>Most Starred</h3>
+      <div>
+        <compose />
+      </div>
+      <div>
+        <list :data="data" type="Post" :option="option" ref="list" :auto-refresh="false" />
+      </div>
+  </div>
+</template>
+
+<script>
+import Compose from '~/components/Compose'
+import List from '~/components/List'
+import api from '~/plugins/api'
+
+export default {
+  async asyncData(ctx) {
+    const option = {
+      include_directed_posts: 1
+    }
+    const data = await api(ctx).fetch(option)
+    return { data, option }
+  },
+  components: {
+    List,
+    Compose
+  },
+  head: {
+    title: 'Most Starred'
+  }
+}
+</script>
